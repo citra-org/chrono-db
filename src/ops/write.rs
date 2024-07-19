1549d17468c5639e4f46d38eba778b90536ccfa1
sugamkuber <sugam7842692@gmail.com>
2024-07-19 21:47:05 +0530

    feat: impr prll w

--- src/ops/write.rs
+++ src/ops/write.rs
@@ -1,42 +1,42 @@
-use std::fs::{OpenOptions, File};
 use std::io::{BufWriter, Write, Result, Error, ErrorKind};
+use std::fs::OpenOptions;
 use std::sync::Arc;
-use std::thread;
+use std::time::Instant;
 use chrono::Utc;
-
-const CHUNK_SIZE: usize = 10000;
+use std::thread;
+use num_cpus;
 
 pub fn write_record(file_path: &str, records: Vec<(String, String)>) -> Result<()> {
-    let record_chunks: Vec<Vec<(String, String)>> = records.chunks(CHUNK_SIZE).map(|chunk| chunk.to_vec()).collect();
+    let start_time = Instant::now();
     let file_path = Arc::new(file_path.to_string());
+    let records = Arc::new(records);
+    let num_cpus = num_cpus::get();
+    let records_per_thread = (records.len() + num_cpus - 1) / num_cpus;
 
     let mut handles = vec![];
-
-    for chunk in record_chunks {
+    for i in 0..num_cpus {
         let file_path = file_path.clone();
-        let chunk = chunk.clone();
-
+        let records = records.clone();
         let handle = thread::spawn(move || {
-            let file = OpenOptions::new().append(true).create(true).open(&*file_path)
+            let start = i * records_per_thread;
+            let end = std::cmp::min((i + 1) * records_per_thread, records.len());
+            let file = OpenOptions::new()
+                .append(true)
+                .create(true)
+                .open(&*file_path)
                 .map_err(|e| Error::new(ErrorKind::PermissionDenied, format!("Failed to open file: {}", e)))?;
             
-            let mut buf_writer = BufWriter::new(file);
-
-            for (header, body) in chunk.iter() {
+            let mut buf_writer = BufWriter::with_capacity(8192, file);
+            for (header, body) in records[start..end].iter() {
                 let time = Utc::now();
                 let combined = format!("{} {} {}\n", time, body, header);
-                let combined_bytes = combined.as_bytes();
-
-                buf_writer.write_all(combined_bytes)
+                buf_writer.write_all(combined.as_bytes())
                     .map_err(|e| Error::new(ErrorKind::WriteZero, format!("Failed to write data into file: {}", e)))?;
             }
-
             buf_writer.flush()
                 .map_err(|e| Error::new(ErrorKind::WriteZero, format!("Failed to flush buffer: {}", e)))?;
-
             Ok(())
         });
-
         handles.push(handle);
     }
 
@@ -45,6 +45,11 @@ pub fn write_record(file_path: &str, records: Vec<(String, String)>) -> Result<(
             return Err(e);
         }
     }
+
+    let duration = start_time.elapsed();
+    println!("Time taken: {:?}", duration);
+    println!("Records written: {}", records.len());
+    println!("Write speed: {} records/second", records.len() as f64 / duration.as_secs_f64());
 
     Ok(())
 }

1f2954a5da8f040bb3cf3b0e7b2a6fb2bef74387
sugamkuber <sugam7842692@gmail.com>
2024-07-20 00:02:10 +0530

    perf: w impr

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+
 mod ops {
     pub mod read;
     pub mod write;
--- src/ops/write.rs
+++ src/ops/write.rs
@@ -12,7 +12,7 @@ pub fn write_record(file_path: &str, records: Vec<(String, String)>) -> Result<(
     let records = Arc::new(records);
     let num_cpus = num_cpus::get();
     let records_per_thread = (records.len() + num_cpus - 1) / num_cpus;
-
+    
     let mut handles = vec![];
     for i in 0..num_cpus {
         let file_path = file_path.clone();
@@ -21,32 +21,32 @@ pub fn write_record(file_path: &str, records: Vec<(String, String)>) -> Result<(
             let start = i * records_per_thread;
             let end = std::cmp::min((i + 1) * records_per_thread, records.len());
             let file = OpenOptions::new()
-                .append(true)
-                .create(true)
-                .open(&*file_path)
-                .map_err(|e| Error::new(ErrorKind::PermissionDenied, format!("Failed to open file: {}", e)))?;
-            
+            .append(true)
+            .open(&*file_path)
+            .map_err(|e| Error::new(ErrorKind::PermissionDenied, format!("Failed to open file: {}", e)))?;
             let mut buf_writer = BufWriter::with_capacity(8192, file);
             for (header, body) in records[start..end].iter() {
                 let time = Utc::now();
                 let combined = format!("{} {} {}\n", time, body, header);
                 buf_writer.write_all(combined.as_bytes())
-                    .map_err(|e| Error::new(ErrorKind::WriteZero, format!("Failed to write data into file: {}", e)))?;
+                .map_err(|e| Error::new(ErrorKind::WriteZero, format!("Failed to write data into file: {}", e)))?;
             }
             buf_writer.flush()
-                .map_err(|e| Error::new(ErrorKind::WriteZero, format!("Failed to flush buffer: {}", e)))?;
+            .map_err(|e| Error::new(ErrorKind::WriteZero, format!("Failed to flush buffer: {}", e)))?;
             Ok(())
         });
         handles.push(handle);
     }
-
+    
     for handle in handles {
         if let Err(e) = handle.join().unwrap() {
             return Err(e);
         }
     }
-
+    
     let duration = start_time.elapsed();
+
+    println!("CPU's: {}",num_cpus);
     println!("Time taken: {:?}", duration);
     println!("Records written: {}", records.len());
     println!("Write speed: {} records/second", records.len() as f64 / duration.as_secs_f64());

bbe8accfb91b33863e1e7346b228d5c935faeaf4
sugamkuber <sugam7842692@gmail.com>
2024-08-06 22:56:07 +0530

    fix: parser fixes

--- src/managers/command/handler.rs
+++ src/managers/command/handler.rs
@@ -1,4 +1,4 @@
-use crate::{ops, managers};
+use crate::{managers, ops};
 use std::io::{Error, ErrorKind, Write};
 use std::net::TcpStream;
 
@@ -7,45 +7,72 @@ pub fn handle_command(
     received: &str,
     chrono: &str,
 ) -> Result<String, Box<dyn std::error::Error + Send + Sync>> {
-    let parts: Vec<&str> = received.split_whitespace().collect();
+    let parts: Vec<&str> = if received.starts_with("INSERT") {
+        let split_parts: Vec<&str> = received.split_whitespace().collect();
+        let middle_body_start = split_parts[0].len();
+        let middle_body_end = received.len()
+            - (split_parts[split_parts.len() - 2].len()
+                + split_parts[split_parts.len() - 1].len()
+                + 2);
+        let middle_body = &received[middle_body_start..middle_body_end].trim();
+        vec![
+            split_parts[0],
+            middle_body,
+            split_parts[split_parts.len() - 2],
+            split_parts[split_parts.len() - 1],
+        ]
+    } else {
+        received.split_whitespace().collect()
+    };
+
     if parts.is_empty() {
         let response_str = "Error: Empty command\n";
         stream.write_all(response_str.as_bytes())?;
         return Ok(response_str.to_string());
     }
+    if !managers::validate::command::validate_commands(parts.clone()) {
+        let error_str = "Error: Invalid command format\n";
+        stream.write_all(error_str.as_bytes())?;
+        return Err(Box::new(Error::new(
+            ErrorKind::InvalidInput,
+            "Invalid command format",
+        )) as Box<dyn std::error::Error + Send + Sync>);
+    }
 
     let response = match parts.as_slice() {
-        ["INSERT", header, body, "INTO", stream_name] => {
-            if managers::validate::command::validate_commands(received) {
-                ops::write::events::write_events(
-                    chrono,
-                    stream_name,
-                    vec![(header.to_string(), body.to_string())],
-                ).map(|_| "OK".to_string())
+        ["INSERT", data, "INTO", stream_name] => {
+            let data = data.trim_matches(|c| c == '{' || c == '}');
+            let events: Vec<(&str, &str)> = data
+                .split("), (")
+                .map(|s| s.trim_matches(|c| c == '(' || c == ')'))
+                .map(|s| {
+                    let parts: Vec<&str> = s.split(", ").collect();
+                    (parts[0].trim_matches('"'), parts[1].trim_matches('"'))
+                })
+                .collect();
+            ops::write::events::write_events(
+                chrono,
+                stream_name,
+                events
+                    .into_iter()
+                    .map(|(header, body)| (header.to_string(), body.to_string()))
+                    .collect(),
+            )
+            .map(|_| "OK".to_string())
+            .map_err(|e| Box::new(e) as Box<dyn std::error::Error + Send + Sync>)
+        }
+        ["SELECT", "*", "FROM", stream_name] => ops::read::events::read_events(chrono, stream_name)
+            .map(|data| format!("{}", data))
+            .map_err(|e| Box::new(e) as Box<dyn std::error::Error + Send + Sync>),
+        ["CREATE", "STREAM", stream_name] => {
+            ops::create::stream::create_stream(chrono, stream_name)
+                .map(|_| "OK".to_string())
                 .map_err(|e| Box::new(e) as Box<dyn std::error::Error + Send + Sync>)
-            } else {
-                Err(Box::new(Error::new(ErrorKind::InvalidInput, "Invalid command format")) as Box<dyn std::error::Error + Send + Sync>)
-            }
         }
-        ["SELECT", "*", "FROM", stream_name] => {
-            if managers::validate::command::validate_commands(received) {
-                ops::read::events::read_events(chrono, stream_name)
-                    .map(|data| format!("{}", data))
-                    .map_err(|e| Box::new(e) as Box<dyn std::error::Error + Send + Sync>)
-            } else {
-                Err(Box::new(Error::new(ErrorKind::InvalidInput, "Invalid command format")) as Box<dyn std::error::Error + Send + Sync>)
-            }
-        }
-        ["CREATE", stream_name] => {
-            if managers::validate::command::validate_commands(received) {
-                ops::create::stream::create_stream(chrono, stream_name)
-                    .map(|_| "OK".to_string())
-                    .map_err(|e| Box::new(e) as Box<dyn std::error::Error + Send + Sync>)
-            } else {
-                Err(Box::new(Error::new(ErrorKind::InvalidInput, "Invalid command format")) as Box<dyn std::error::Error + Send + Sync>)
-            }
-        }
-        _ => Err(Box::new(Error::new(ErrorKind::InvalidInput, "Unknown command")) as Box<dyn std::error::Error + Send + Sync>),
+        _ => Err(
+            Box::new(Error::new(ErrorKind::InvalidInput, "Unknown command"))
+                as Box<dyn std::error::Error + Send + Sync>,
+        ),
     };
 
     match response {
--- src/managers/validate/command.rs
+++ src/managers/validate/command.rs
@@ -1,47 +1,56 @@
 use crate::managers;
+use regex::Regex;
 use std::sync::Arc;
 
-pub fn validate_commands(command: &str) -> bool {
-    let parts: Vec<&str> = command.split_whitespace().collect();
-    
-    fn is_stream_valid(name: &str) -> bool {
-        is_stream_name_valid(name) && is_stream_exists(name)
-    }
-
-    fn is_stream_name_valid(name: &str) -> bool {
-        !name.is_empty() 
-        && name.chars().next().map_or(false, |c| c.is_ascii_lowercase() || c == '_')
-        && name.chars().all(|c| c.is_ascii_lowercase() || c.is_ascii_digit() || c == '_')
-        && !name.starts_with('_')
-        && !name.ends_with('_')
-    }
-    
-    fn is_stream_exists(name: &str) -> bool {
-        let file_path = Arc::new(format!(
-            "/var/lib/citra/chrono/{}/{}.chrono",
-            "chrono", name
-        ));
-        match managers::files::check::check_file(&file_path, true) {
-            Ok(_) => true,
-            Err(_) => false,
-        }
-    
-    }
-
+pub fn validate_commands(parts: Vec<&str>) -> bool {
     match parts.as_slice() {
-        ["INSERT", header, body, "INTO", stream] => {
-            is_stream_valid(&stream.to_lowercase()) &&
-            header.starts_with('"') && header.ends_with('"') &&
-            body.starts_with('"') && body.ends_with('"')
-        },
-        ["SELECT", "*", "FROM", stream]  => {
+        ["INSERT", event, "INTO", stream] => {
+            is_stream_valid(&stream.to_lowercase()) && is_event_valid(event)
+        }
+        ["SELECT", "*", "FROM", stream] => {
             is_stream_valid(&stream.to_lowercase())
             // | ["SELECT", value, "FROM", stream]
             // (value == "*" || (value.starts_with('"') && value.ends_with('"')))
-        },
-        ["CREATE", stream] => {
-            is_stream_valid(&stream.to_lowercase())
-        },
+        }
+        ["CREATE", "STREAM", stream] => is_stream_name_valid(&stream.to_lowercase()),
         _ => false,
     }
+}
+
+fn is_stream_valid(name: &str) -> bool {
+    is_stream_name_valid(name) && is_stream_exists(name)
+}
+
+fn is_stream_name_valid(name: &str) -> bool {
+    !name.is_empty()
+        && name
+            .chars()
+            .next()
+            .map_or(false, |c| c.is_ascii_lowercase() || c == '_')
+        && name
+            .chars()
+            .all(|c| c.is_ascii_lowercase() || c.is_ascii_digit() || c == '_')
+        && !name.starts_with('_')
+        && !name.ends_with('_')
+}
+
+fn is_stream_exists(name: &str) -> bool {
+    let file_path = Arc::new(format!(
+        "/var/lib/citra/chrono/{}/{}.chrono",
+        "chrono", name
+    ));
+    match managers::files::check::check_file(&file_path, true) {
+        Ok(_) => true,
+        Err(_) => false,
+    }
+}
+fn is_event_valid(event: &str) -> bool {
+    let single_tuple_pattern = r#"\(\s*"\w+"\s*,\s*"\w+"\s*\)"#;
+    let multiple_tuples_pattern =
+        r#"\{\s*(\(\s*"\w+"\s*,\s*"\w+"\s*\)\s*,\s*)*\(\s*"\w+"\s*,\s*"\w+"\s*\)\s*\}"#;
+
+    let single_tuple_regex = Regex::new(single_tuple_pattern).unwrap();
+    let multiple_tuples_regex = Regex::new(multiple_tuples_pattern).unwrap();
+
+    single_tuple_regex.is_match(event) || multiple_tuples_regex.is_match(event)
 }
